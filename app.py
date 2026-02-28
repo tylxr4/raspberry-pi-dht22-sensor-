e3cfe7265003ca71498aa774fd2f700f91ab71e5
tylxr4 <summerlintyler010@gmail.com>
2026-02-28 10:49:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -79,6 +79,8 @@ def get_alerts():
     else:
         return jsonify([])
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+import os
 
+if __name__ == '__main__':
+    debug = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
+    app.run(host='0.0.0.0', port=5000, debug=debug)
